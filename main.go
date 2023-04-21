ac0ca1380f0a139eb760cd7d65945cec45a3d961
VMTask <108920745+VMTask@users.noreply.github.com>
2023-04-21 06:11:02 +0000

    Add image proxy feature.🎉

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	})
 
 	handler.GET("/edgesvc/turing/conversation/create", proxy)
+	handler.GET("/images/create",image_proxy)
 
 	handler.OPTIONS("/edgesvc/turing/conversation/create", func(ctx *gin.Context) {
 		ctx.Header("Access-Control-Allow-Origin", "*")
@@ -40,6 +41,13 @@ func main() {
 		ctx.Header("Access-Control-Allow-Headers", "*")
 		ctx.JSON(200, gin.H{"message": "ok"})
 	})
+	handler.OPTIONS("/images/create", func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Methods", "*")
+		ctx.Header("Access-Control-Allow-Headers", "*")
+		ctx.JSON(200, gin.H{"message": "ok"})
+	})
+
 
 	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handler)
 }
@@ -86,3 +94,46 @@ func proxy(c *gin.Context) {
 	})
 
 }
+func image_proxy(c *gin.Context) {
+
+	var url string
+	var err error
+	var request_method string
+	var request *http.Request
+	var response *http.Response
+
+	url = "https://www.bing.com/images/create"
+	request_method = c.Request.Method
+
+	request, err = http.NewRequest(request_method, url, c.Request.Body)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Copy headers
+	for key, value := range c.Request.Header {
+		request.Header.Set(key, value[0])
+	}
+
+	response, err = client.Do(request)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	defer response.Body.Close()
+	c.Header("Content-Type", response.Header.Get("Content-Type"))
+	// Add CORS
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	// Get status code
+	c.Status(response.StatusCode)
+	c.Stream(func(w io.Writer) bool {
+		// Write data to client
+		io.Copy(w, response.Body)
+		return false
+	})
+
+}
+
